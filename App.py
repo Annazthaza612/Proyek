8c555bb9058fbf959c2b2118252d4cfd4ece22ee
Annazthaza612 <annazthaza612@students.unnes.ac.id>
2025-06-05 07:43:43 +0700

    Update App.py

--- App.py
+++ App.py
@@ -7,5 +7,5 @@ st.sidebar.header("Main Menu")
 
 #Memberikan Judul dan Gambar yang menarik
 st.title("APLIKASI MENGUKUR TINGKAT STRES")
-st.image ("https://pngtree.com/freepng/stressed-office-workers-burnout-open-laptops-desks-flat-colors-minimalist-frustration-exhaustion-corporate-vector-workplace-stress-deadline-pressure_20454278.html")
+st.image ("https://png.pngtree.com/png-vector/20220126/ourmid/pngtree-stressed-businessman-and-burnout-at-work-with-deadline-pressure-vector-png-image_44232710.jpg")
 st.write ("Dibuat Oleh Kelompok 16")
